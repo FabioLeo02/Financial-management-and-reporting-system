ac02a37b04dce1c943626a5ed088e892042ea60d
FabioLeo02 <fabioleandroti@gmail.com>
2023-11-11 14:02:40 -0300

    added: feat report responsiveness and form page creation

---
+++ js/janelaform.js
@@ -0,0 +1,7 @@
+document.getElementById('novoRelatorioBtn').addEventListener('click', function() {
+    document.getElementById('sidebar').style.right = '0';
+});
+
+document.getElementById('fecharSidebarBtn').addEventListener('click', function() {
+    document.getElementById('sidebar').style.right = '-80%';
+});
